e470b42e621ed9ce389e7bf0601f635040c69b70
Laetitia de Bardin <titiluna.sky85@gmail.com>
2020-07-23 17:29:39 +0200

    step1

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+let MongoClient = require("mongodb").MongoClient;
+let url = "mongodb://localhost:27017/dbgenerator";
+
+let db 
+const dbName = "dbgenerator";
+
+MongoClient.connect(url, function(err, client) {
+    if (err) return console.log(err);
+    console.log("Database created!")
+    
+    db = client.db(dbName);
+
+    db.createCollection("students", function(err, res) {
+        if (err) throw err;
+        console.log("collection created !");
+        client.close();
+    });
+
+    db.createCollection("group", function(err, res) {
+        if (err) throw err;
+        console.log("collection created !");
+        client.close();
+});
+});
