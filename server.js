08da4617b2951686379107b2a5ba7b56899fd8ca
Hugo Barnas <hugo.barnas@gmail.com>
2020-07-27 17:21:32 +0200

    list students from mongodb

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-const fetch = require ("node-fetch");
+const fetch = require("node-fetch");
+const fs = require("fs");
+const ejs = require("ejs");
+const qs = require('querystring');
+
+let students = [];
 
 
 
@@ -11,9 +16,20 @@ app.get('/students', async function (req, res) {
     let rec = await fetch(`http://localhost:8000/students`);
     let testRec = await rec.json();
     res.status(200);
-    res.send(testRec);
+    //res.send(testRec);
+
+
+    const ejs_file = fs.readFileSync(__dirname + '/index.ejs', 'utf-8');
+    const html = ejs.render(ejs_file, {
+        studentsAAA: testRec
+    });
+    res.send(html);
 
 });
+
+app.post('/student', async function (req, res) {
+
+})
 
 
 //ECOUTE DU PORT
